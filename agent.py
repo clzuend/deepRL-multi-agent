c696a3ba4219ba8b2d30692067ddfe693e0e001d
Christian L. Zuend <clzuend@gmail.com>
2020-05-11 18:46:26 +0200

    fix typos

--- agent.py
+++ agent.py
@@ -12,7 +12,7 @@ from torch.autograd import Variable
 from torchviz import make_dot
 
 BUFFER_SIZE = int(1e5)  # replay buffer size
-BATCH_SIZE = 128       # minibatch size
+BATCH_SIZE = 128        # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
 LR_ACTOR = 1e-4         # learning rate of the actor 
